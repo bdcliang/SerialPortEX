0a7e27802b66820e794788a534fa7f1c9a05bab4
Delphi <delphi_bigd@outlook.com>
2016-03-20 03:39:36 +0800

    add more class

---
+++ SerialPortEX/LLSerialPortFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerialPortEX
+{
+    /// <summary>
+    /// this the factory method to generate the instance
+    /// </summary>
+    class LLSerialPortFactory
+    {
+        static LLSerialPortDriver _llserial = new LLSerialPortDriver();
+        public static LLSerialPortDriver CreateInstance()
+        {
+            return _llserial;
+        }
+    }
+}
